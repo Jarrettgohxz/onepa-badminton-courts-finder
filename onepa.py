ff8d4b876e4e2f3537d1ec4da4e455ede8c4bf02
Jarrett Goh <Jarrettgoh.xz@gmail.com>
2023-11-20 16:01:03 +0800

    #

--- onepa.py
+++ onepa.py
@@ -5,7 +5,7 @@ import re
 
 
 class OnePA():
-    # get_courts_availability_url = "https://www.onepa.gov.sg/pacesapi/facilityavailability/GetFacilitySlots?selectedFacility=canberracc_BADMINTONCOURTS&selectedDate=20/10/2023"
+    # get_courts_availability_url = "https://www.onepa.gov.sg/pacesapi/facilityavailability/GetFacilitySlots?selectedFacility=canberracc_BADMINTONCOURTS&selectedDate=20/11/2023"
 
     #   "https://www.onepa.gov.sg/facilities/availability?facilityId=siglapcc_BADMINTONCOURTS&date=18/11/2023&time=all",
 
@@ -25,79 +25,39 @@ class OnePA():
     def get_venues_list(self):
         # 1 to 10
         page = 1
+        count = 0
 
-        # r = self.s.get(
-        #     f'https://www.onepa.gov.sg/pacesapi/facilitysearch/searchjson?facility=BADMINTON%20COURTS&outlet=&date=&time=&page={page}&division=')
+        # while True:
 
         r = self.s.get(
-            'https://www.onepa.gov.sg/pacesapi/catalogs/outlets',
-            # 'https://www.onepa.gov.sg/pacesapi/FacilitySearch/GetFacilityOutlets',
-            headers={
-                'Content-Type': 'application/json;charset=utf-8',
-                "Accept": "application/json, text/plain, */*",
+            f'https://www.onepa.gov.sg/pacesapi/facilitysearch/searchjson?facility=BADMINTON%20COURTS&outlet=&date=&time=&page={page}&division=')
 
-                # else won't work - empty response
-                'Referer': 'https://www.onepa.gov.sg/facilities/search?facility=BADMINTON%20COURTS'
-            })
+        with open('data.json', 'w') as w:
+            w.write(r.text)
 
-        # outlets = json.loads(r.text)['data']['data']['outlets']
+            # res_json = json.loads(r.text)
+            # outlets = res_json['data']['results']
 
-        # index = 0
+            # if len(outlets) == 0:
+            #     break
 
-        # for outlet in outlets:
-        #     for i in outlet['ccList']:
-        #         label = i['label']
-
-        #         if 'rc' in label.lower() or 'rn' in label.lower():
-        #             continue
-
-        #         print(i['label'])
-
-        #         index += 1
-        #         print(index)
-
-        outlets = json.loads(r.text)['data']['outlets']
-
-        index = 0
-
-        for outlet in outlets:
-            title = outlet['title']
-
-            if outlet['type'] != 'CC':
-                continue
-
-            if re.search('cc$', title.lower(), re.IGNORECASE) == None:
-                continue
-
-            if 'defunct' in title.lower():
-                continue
-
-            print(outlet['title'])
-            index += 1
-            print(index)
-
-        with open('venues2.json', 'w', encoding='utf-8') as v:
-            v.write(r.text)
+            # page += 1
 
     def get_courts_availability(self):
 
-        facility = urllib.parse.quote('BADMINTON COURTS')
-        date = '23/11/2023'
-        outlet = urllib.parse.quote(
-            'CANBERRA CC,YEW TEE CC,YUHUA CC, BIDADARI CC, ZHENGHUA CC')
-
-        get_courts_list_api = f'https://www.onepa.gov.sg/pacesapi/facilitysearch/searchjson?facility={
-            facility}&outlet={outlet}&date={date}&time=all&page=1&division='
+        self.s.get(
+            'https://www.onepa.gov.sg/facilities/search?facility=BADMINTON%20COURT')
 
         res = self.s.get(
-            get_courts_list_api,
+            'https://www.onepa.gov.sg/pacesapi/facilityavailability/GetFacilitySlots?selectedFacility=canberracc_BADMINTONCOURTS&selectedDate=20/11/2023',
             headers={
+                'Content-Type': 'application/json;charset=utf-8',
                 "Accept": "application/json, text/plain, */*",
-                # "Referer": "https://www.onepa.gov.sg/facilities",
+                'Referer': 'https://www.onepa.gov.sg/facilities/search?facility=BADMINTON%20COURTS'
             }
         )
 
-        with open("data.json", "w", encoding="utf-8") as test:
+        with open("courts.json", "w", encoding="utf-8") as test:
             test.write(res.text)
 
     def remove_first_few_elements(self, arr, chunk):
@@ -117,7 +77,7 @@ class OnePA():
 if __name__ == "__main__":
     onepa = OnePA()
 
-    # onepa.set_cookies()
+    onepa.set_cookies()
     # onepa.test_algo()
-    onepa.get_venues_list()
-    # onepa.get_courts_availability()
+    # onepa.get_venues_list()
+    onepa.get_courts_availability()
